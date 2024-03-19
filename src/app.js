80ac4500a8ba030b82f615bdcdd4eb61ac6e905b
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2024-03-19 09:53:58 -0300

    update

--- src/app.js
+++ src/app.js
@@ -13,15 +13,11 @@ const noticias = [
     {id: 4, tema: 'Sérvia', publicacao: 'G'}
 ]
 
-//vai retornar o objeto através de uma busca por id
-function buscarNoticiaPorId(id){
-    return noticias.filter( posts => posts.id == id)
-}
-
-//vai procurar o index da notícia no array através do id
-function buscarIndexNoticia(id){
-    return noticias.findIndex(noticias => noticias.id == id)
-}
+//Rota para cadastrar novas noticias
+app.post('/noticias', (req, res) => {
+    noticias.push(req.body)
+    res.status(201).send('Notícia cadastrada com sucesso!')
+})
 
 //criar rota raiz
 app.get('/', (req, res) => {
@@ -36,15 +32,29 @@ app.get('/noticias/:id', (req, res) => {
     res.json(buscarNoticiaPorId(req.params.id))
 })
 
-app.post('/noticias', (req, res) => {
-    noticias.push(req.body)
-    res.status(201).send('Notícia cadastrada com sucesso!')
+//Rota para editar uma noticia
+app.put('/noticias/:id', (req, res) => {
+    let index = buscarIndexNoticia(req.params.id)
+    noticias[index].tema = req.body.tema
+    noticias[index].publicacao = req.body.publicacao
+    res.json(noticias)
 })
 
+//Rota para deletar uma noticia
 app.delete('/noticias/:id', (req, res) => {
     let index = buscarIndexNoticia(req.params.id)
     noticias.splice(index, 1)
     res.send(`Notícia com id ${req.params.id} excluída com sucesso!`)
 })
+
+//vai retornar o objeto através de uma busca por id
+function buscarNoticiaPorId(id){
+    return noticias.filter( posts => posts.id == id)
+}
+
+//vai procurar o index da notícia no array através do id
+function buscarIndexNoticia(id){
+    return noticias.findIndex(noticias => noticias.id == id)
+}
 
 export default app
