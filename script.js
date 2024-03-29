9936f89ff6464653466d2d18bcc69dffd1a10679
José Ednardo Dantas Neto <ednardoneto22@gmail.com>
2024-03-29 16:03:36 -0300

    commit

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ function updateRecentNewsList(noticias) {
     const recentPostsDiv = document.querySelector('.postsrecents')
     recentPostsDiv.innerHTML = '';
 
-    const startIndex = noticias.length > 3 ? noticias.length - 3 : 0;
+    const startIndex = noticias.length > 3 ? noticias.length - 3 : 0
 
     for (let i = startIndex; i < noticias.length; i++) {
-        const noticia = noticias[i];
+        const noticia = noticias[i]
 
         const newsItem = document.createElement('div')
         newsItem.className = 'news-item'
@@ -74,16 +74,57 @@ async function addPublish(event) {
 async function loadNewsDetails() {
     try{
         const response = await axios.get(`http://localhost:3000/noticias`)
-        const noticia = response.data
-        const ultimaNoticia = noticia[noticia.length - 1]
+        const noticias = response.data
+        const ultimasNoticias = noticias[noticias.length - 1]
         
-        document.getElementById("tema").textContent = ultimaNoticia.tema || "Título não disponível"
-        document.getElementById("msg").textContent = ultimaNoticia.noticia
-        if(noticia.imagem && document.querySelector('.postImage')){
-            document.querySelector('.postImage').src = noticia.imagem
+        document.getElementById("tema").textContent = ultimasNoticias.tema || "Título não disponível"
+        document.getElementById("msg").textContent = ultimasNoticias.noticia
+        if(noticias.imagem && document.querySelector('.postImage')){
+            document.querySelector('.postImage').src = noticias.imagem
         }
     }catch(error){
         console.error('Erro ao carregar os detalhes da notícia', error)
+    }
+}
+
+async function deletePublish(event) {
+    event.preventDefault()
+    console.log('Função deletePublish chamada!')
+    try {
+        const get = await axios.get(`http://localhost:3000/noticias`)
+        const noticias = get.data
+        const idUltimaNoticia = noticias[noticias.length - 1].id
+        const response = await axios({
+            method: "DELETE",
+            url: `http://localhost:3000/noticias/` + idUltimaNoticia,
+            headers: {"Content-Type": "application/json"}
+        });
+        console.log('Dados removidos', response.data)
+        window.location.reload()
+    } catch (error) {
+        console.error("Não foi possível remover", error)
+    }
+}
+
+async function updatePublish(event) {
+    event.preventDefault()
+    console.log('Função updatePublish chamada!')
+    const title = document.getElementById('title').value
+    const publication = document.getElementById('publication').value
+    try{
+        const get = await axios.get(`http://localhost:3000/noticias`)
+        const noticias = get.data
+        const idUltimaNoticia = noticias[noticias.length - 1].id
+        const response = await axios({
+            method: "PUT",
+            url: `http://localhost:3000/noticias/` + idUltimaNoticia,
+            data: {tema: title, noticia: publication},
+            headers: {"Content-Type": "application/json"}
+        })
+        console.log('Dados alterados', response.data)
+        window.location.reload()
+    }catch (error){
+        console.error("Não foi possível alterar os dados", error)
     }
 }
 
